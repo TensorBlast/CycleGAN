9daab50b55a5797f9ebac395db8a9bf6e39262d8
Ankit Pasi <ankitpasi1@gmail.com>
2024-04-23 23:08:59 +0000

    Fixed Options Issue

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from typing import List, Union, Optional
 @dataclass
 class Options:
     # Required options
-    dataroot: str  # Path to images
+    dataroot: str # Path to images
 
     # Options with default values
     name: str = "experiment_name"
@@ -63,6 +63,7 @@ class Options:
     pool_size: int = 50
     lr_policy: str = "linear"
     lr_decay_iters: int = 50
+    isTrain: bool = True
 
     results_dir: str = "./results/"
     aspect_ratio: float = 1.0
@@ -72,7 +73,43 @@ class Options:
     # Overriding load_size for test phase
     load_size: int = field(default=256)
 
+    use_wandb: bool = False  # Default: if specified, then init wandb logging
+    wandb_project_name: str = "CycleGAN-and-pix2pix"
+
     def __post_init__(self):
         # Convert comma-separated gpu_ids to a list of integers
         if isinstance(self.gpu_ids, str):
             self.gpu_ids = [int(id_) for id_ in self.gpu_ids.split(',') if id_.strip().isdigit()]
+
+        if self.isTrain:
+            self.lambda_A : float = 10
+            self.lambda_B : float = 10
+            self.lambda_identity : float = 0.5
+
+
+@dataclass
+class TrainOptions:
+    display_freq: int = 400
+    display_ncols: int = 4
+    display_id: int = 1
+    display_server: str = "http://localhost"
+    display_env: str = "main"
+    display_port: int = 8097
+    update_html_freq: int = 1000
+    print_freq: int = 100
+    no_html: bool = False
+    save_latest_freq: int = 5000
+    save_epoch_freq: int = 5
+    save_by_iter: bool = False
+    continue_train: bool = False
+    epoch_count: int = 1
+    phase: str = "train"
+    n_epochs: int = 60
+    n_epochs_decay: int = 40
+    beta1: float = 0.5
+    lr: float = 0.0002
+    gan_mode: str = "lsgan"
+    pool_size: int = 50
+    lr_policy: str = "linear"
+    lr_decay_iters: int = 50
+    isTrain: bool = True
